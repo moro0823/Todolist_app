7a5e3f5372c36549f0b13e97eae00becf3ef65d9
EC2 Default User <ec2-user@ip-172-31-49-42.ec2.internal>
2021-04-11 23:00:33 +0000

    NEXT 検索機能

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,5 +1,6 @@
 class ToDosController < ApplicationController
   def index
+    @to_dos = ToDo.all
   end
 
 
@@ -7,26 +8,26 @@ class ToDosController < ApplicationController
     @to_do = ToDo.new(to_do_params)
     @to_do.user_id = current_user.id
     @to_do.save
-    @to_dos = ToDo.all
+    @user = current_user
+    @to_dos = @user.to_dos
   end
 
   def edit
     @to_do = ToDo.find(params[:id])
-    @to_do.update(to_do_params)
-    @to_dos = ToDo.all
   end
 
   def update
     @to_dos = ToDo.all
     @to_do = ToDo.find(params[:id])
-    @to_do.user_id = current_user.id
     @to_do.update(to_do_params)
+    redirect_to user_path
   end
 
   def destroy
     to_do = ToDo.find(params[:id])
     to_do.destroy
     @to_dos = ToDo.all
+    redirect_to to_dos_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @to_dos = ToDo.all
+    @to_dos = @user.to_dos
     @to_do = ToDo.new
+    @to_do.user_id = current_user
   end
 
   def create
